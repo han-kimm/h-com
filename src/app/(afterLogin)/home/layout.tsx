d4e199d84abe74b8762663937544ffd973147959
Haneul-Kim <4188989@naver.com>
2024-01-18 12:11:24 +0900

    feat: 라우터 그룹 형성

---
+++ src/app/(afterLogin)/home/layout.tsx
@@ -0,0 +1,10 @@
+const HomeLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div>
+      홈 레이아웃
+      {children}
+    </div>
+  );
+};
+
+export default HomeLayout;
---
+++ src/app/(afterLogin)/layout.tsx
@@ -0,0 +1,5 @@
+const AfterLoginLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div>로그인 후 레이아웃 {children}</div>;
+};
+
+export default AfterLoginLayout;
